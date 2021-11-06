9399f75a5f8d00c91ba8d210099c4687cd9c813a
Horace <pifi77@hotmail.fr>
2021-11-06 12:47:47 +0100

    Focus Cam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : InterestPoint
+public class Player : MonoBehaviour
 {
     [Header("Movement")]
     public float speedNormal = 1;
     private Vector2 lastSpeed = Vector2.zero;
     public float friction = 0.9f;
 
+    public Transform camFocus;
+    public float ecart = 0.5f;
+    public float camFocusDelay = 0.5f;
+    private Vector2 lastDirection = Vector2.zero;
+    private Vector2 targetDirection = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -45,7 +50,7 @@ public class Player : InterestPoint
         }
 
 
-        Vector2 finalSpeed = direction.normalized* speedNormal;
+        Vector2 finalSpeed = direction.normalized * speedNormal;
 
         //compute speed !! 
         lastSpeed += finalSpeed * Time.deltaTime;
@@ -53,7 +58,12 @@ public class Player : InterestPoint
 
         Vector3 res = new Vector3(lastSpeed.x, lastSpeed.y, this.transform.position.z);
         this.transform.position += res * Time.deltaTime;
+        if (direction != Vector2.zero)
+            targetDirection = direction.normalized;
 
+        lastDirection = Vector2.Lerp(lastDirection, targetDirection, Time.deltaTime * camFocusDelay);
+        camFocus.position = this.transform.position + (Vector3)lastDirection * ecart;
+        //        camFocus.position = Vector3.Lerp(camFocus.position, this.transform.position + (Vector3)lastDirection * ecart, Time.deltaTime * camFocusDelay);
     }
 
 
@@ -62,4 +72,5 @@ public class Player : InterestPoint
         if (Input.GetKeyDown(KeyCode.R))
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
+
 }
